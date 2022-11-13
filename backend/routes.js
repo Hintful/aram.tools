2f6411193dc0821555b2e8a1ae63ebb3f899c110
Youngmok Cho <choym0098@gmail.com>
2022-11-13 03:12:17 -0500

    added endpoint for latest match ids

--- backend/routes.js
+++ backend/routes.js
@@ -4,10 +4,14 @@ const router = express.Router()
 const {
     userName,
     userInfo,
+    matchNum,
+    latestMatches,
 } = require('./controllers');
 
-router.get("/lol/summoner/info/:userName", userInfo)
+router.get("/lol/summoner/:userName/info", userInfo)
+router.get("/lol/summoner/:userName/latest-matches/:matchNum", latestMatches)
 
 router.param("userName", userName);
+router.param("matchNum", matchNum)
 
 module.exports = router;
