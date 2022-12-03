1480a0133d068dd2078deecb71cd0b53f2c80e26
Youngmok Cho <choym0098@gmail.com>
2022-12-03 02:33:17 -0500

    a

--- webserver/controllers.js
+++ webserver/controllers.js
@@ -11,7 +11,7 @@ const ARAM_QUEUE_ID = 450;
 const MAX_MATCH_NUM = 100;
 
 exports.userName = async (req, res, next, name) => {
-    FULL_ENDPOINT = process.env.NA_BASE_URL + "lol/summoner/v4/summoners/by-name/" + name + "?api_key=" + process.env.API_KEY
+    FULL_ENDPOINT = process.env.NA_BASE_URL + "lol/summoner/v4/summoners/by-name/" + name + "?api_key=" + process.env.RIOT_API_KEY
     const leagueResp = await fetch(FULL_ENDPOINT, options);
     const userInfo = await leagueResp.json();
     req.userInfo = userInfo;
@@ -34,28 +34,28 @@ exports.userInfo = (req, res) => {
 
 exports.latestMatches = async (req, res) => {
     FULL_ENDPOINT = process.env.AMERICAS_MATCH_BASE_URL + `lol/match/v5/matches/by-puuid/${req.userInfo.puuid}/ids` +
-        `?queue=${ARAM_QUEUE_ID}&type=normal&count=${req.matchNum}&api_key=` + process.env.API_KEY
+        `?queue=${ARAM_QUEUE_ID}&type=normal&count=${req.matchNum}&api_key=` + process.env.RIOT_API_KEY
     const matchResp = await fetch(FULL_ENDPOINT, options)
     const matchIds = await matchResp.json()
     return res.json(matchIds)
 }
 
 exports.matchInfo = async (req, res) => {
-    FULL_ENDPOINT = process.env.AMERICAS_MATCH_BASE_URL + `lol/match/v5/matches/${req.matchId}` + "?api_key=" + process.env.API_KEY
+    FULL_ENDPOINT = process.env.AMERICAS_MATCH_BASE_URL + `lol/match/v5/matches/${req.matchId}` + "?api_key=" + process.env.RIOT_API_KEY
     const matchResp = await fetch(FULL_ENDPOINT, options)
     const matchData = await matchResp.json()
     return res.json(matchData)
 }
 
 exports.challengesPercentile = async (req, res) => {
-    FULL_ENDPOINT = process.env.NA_BASE_URL + `lol/challenges/v1/challenges/percentiles` + "?api_key=" + process.env.API_KEY
+    FULL_ENDPOINT = process.env.NA_BASE_URL + `lol/challenges/v1/challenges/percentiles` + "?api_key=" + process.env.RIOT_API_KEY
     const response = await fetch(FULL_ENDPOINT, options)
     const data = await response.json()
     return res.json(data)
 }
 
 exports.challengesData = async (req, res) => {
-    FULL_ENDPOINT = process.env.NA_BASE_URL + `lol/challenges/v1/player-data/${req.userInfo.puuid}` + "?api_key=" + process.env.API_KEY
+    FULL_ENDPOINT = process.env.NA_BASE_URL + `lol/challenges/v1/player-data/${req.userInfo.puuid}` + "?api_key=" + process.env.RIOT_API_KEY
     const response = await fetch(FULL_ENDPOINT, options)
     const data = await response.json()
     return res.json(data)
